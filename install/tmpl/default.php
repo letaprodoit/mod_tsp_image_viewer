1438533a0e874805d3dc995ca794b73b54ed37b0
Sharron Denice <sharron@me.com>
2013-08-05 20:45:34 -0500

    Fixed invalid chars in copyright Ticket #IVM-2

--- install/tmpl/default.php
+++ install/tmpl/default.php
@@ -6,7 +6,7 @@
  * @filename	default.php
  * @version		1.0.0
  * @author		Sharron Denice, The Software People, LLC on 2013/02/09
- * @copyright	Copyright ï¿½ 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
+ * @copyright	Copyright © 2013 The Software People, LLC (www.thesoftwarepeople.com). All rights reserved
  * @license		GNU Public License v3.0 (http://www.gnu.org/licenses/gpl-3.0.html)
  * @brief		default template
  * 
